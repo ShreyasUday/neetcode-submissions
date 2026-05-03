d37e08fceef2ef06f6140852938f642dd79242c0
Shreyas Uday <143956841+ShreyasUday@users.noreply.github.com>
2026-05-04 00:49:35 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.java
@@ -0,0 +1,67 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int low = 0;
+        int high = s.length() - 1;
+        int lifeLine = 0;
+
+        while (low <= high) {
+            if (s.charAt(low) == s.charAt(high)) {
+                low++;
+                high--;
+                continue;
+            } else {
+                // int i1 = low;
+                // int j1 = high;
+                // Boolean check1 = true;
+                // while (i1 < j1) {
+                //     if (!(s.charAt(i1 + 1) == s.charAt(j1))) {
+                //         check1 = false;
+                //         break;
+                //     }
+                //     i1++;
+                //     j1--;
+                // }
+                // int i2 = low;
+                // int j2 = high;
+                // Boolean check2 = true;
+                // while (i2 < j2) {
+                //     if (!(s.charAt(i2) == s.charAt(j2 - 1))) {
+                //         check2 = false;
+                //         break;
+                //     }
+                //     i2++;
+                //     j2--;
+                // }
+                return check(s,low+1,high) || check(s,low,high-1);
+                // if(lifeLine == 0){
+                //     if(s.charAt(low+1) == s.charAt(high)){
+                //         low++;
+                //         lifeLine++;
+                //     }
+                //     else if(s.charAt(low) == s.charAt(high-1)){
+                //         high--;
+                //         lifeLine++;
+                //     }
+                //     else{
+                //         return false;
+                //     }
+                // }
+                // else{
+                //     return false;
+                // }
+            }
+        }
+        return true;
+    }
+
+    public Boolean check(String s, int low, int high) {
+        while(low<high){
+            if (s.charAt(low) != s.charAt(high)) {
+                return false;
+            }
+            low++;
+            high--;
+        }
+        return true;
+    }
+}
