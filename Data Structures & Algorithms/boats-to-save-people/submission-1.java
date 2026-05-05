75eb74ab377ad4ca1e90a0d7350e323d64a4a3d0
Shreyas Uday <143956841+ShreyasUday@users.noreply.github.com>
2026-05-06 02:02:01 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int n = people.length;
+        int low = 0;
+        int high = n-1;
+
+        int res = 0;
+
+        Arrays.sort(people);
+
+        while(low<=high){
+            if(people[low] + people[high] <= limit){
+                res++;
+                low++;
+                high--;
+            }
+            else{
+                res++;
+                high--;
+            }
+        }
+        return res;
+    }
+}
