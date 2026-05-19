562f078fe5fc7e7c1e72cd20e6e230be2d7b7c96
Shreyas Uday <143956841+ShreyasUday@users.noreply.github.com>
2026-05-19 23:43:52 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        Stack<int[]> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int t = temperatures[i];
+            while(!stack.isEmpty() && t>stack.peek()[0]){
+                int [] temp = stack.pop();
+                res[temp[1]] = i-temp[1];
+            }
+            stack.push(new int[]{t,i});
+        }
+        return res;
+    }
+}
